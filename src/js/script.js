fc621dd661e7104efed8054dc3f327eb757cbebe
peppe-quint-boogaard <peppequintb@gmail.com>
2019-03-04 15:41:28 +0100

    created object with data

--- src/js/script.js
+++ src/js/script.js
@@ -7,8 +7,28 @@ import { API } from "/node_modules/oba-wrapper/js/index.js";
     key: "1e19898c87464e239192c8bfe422f280"
   });
 
-  const request = await api.createIterator("search/banaan");
+  const request = await api.createIterator(
+    "search/briefkaart&facet=type(postcard)"
+  );
   for await (const response of request) {
-    console.log(response);
+    const results = response.map(result => object(result));
+    console.log(results);
+    renderData(results);
   }
 })();
+
+const object = data => {
+  const createObject = {
+    title: data.title.full,
+    publication: data.publication.year,
+    image: data.images[0]
+  };
+  return createObject;
+};
+
+const renderData = data => {
+  const main = document.querySelector("main");
+  const postcard = document.createElement("div");
+  postcard.innerHTML = `${data[0].title}`;
+  main.appendChild(postcard);
+};
