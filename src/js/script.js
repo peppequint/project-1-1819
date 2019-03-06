4e6c65b2001653516e75936b3f07605352102117
peppe-quint-boogaard <peppequintb@gmail.com>
2019-03-06 15:26:35 +0100

    router and object literals (not finished)

--- src/js/script.js
+++ src/js/script.js
@@ -1,51 +1,79 @@
 import { API } from "/node_modules/oba-wrapper/js/index.js";
 
-(async () => {
-  localStorage.clear();
+const app = {
+  call: (async () => {
+    const api = new API({
+      key: "1e19898c87464e239192c8bfe422f280"
+    });
 
-  const api = new API({
-    key: "1e19898c87464e239192c8bfe422f280"
-  });
-
-  const request = await api.createIterator(
-    "search/gracht&facet=type(postcard)"
-  );
-  for await (const response of request) {
-    // console.log(response);
-    const results = response.map(result => createObject(result));
-    renderData(results);
-  }
-})();
+    const request = await api.createIterator(
+      "search/water&facet=type(postcard)"
+    );
+    for await (const response of request) {
+      const results = response.map(result => createObject(result));
+      console.table(results);
+      render.overview(results);
+    }
+  })()
+};
 
 const createObject = data => {
   const object = {
-    title: data.titles.title._text,
-    subject: data.subjects.subject._text,
+    title: data.titles.title._text
+      ? data.titles.title._text
+      : data.titles.title[1]._text,
+    subject: !data.subjects
+      ? "Plaats onbekend"
+      : data.subjects.subject._text
+      ? data.subjects.subject._text
+      : data.subjects.subject[1]._text,
     publication: data.publication
       ? data.publication.year._text
-      : "Jaar niet bekend",
+      : "Jaar onbekend",
     image: data.coverimages.coverimage._text
   };
+
   return object;
 };
 
-const renderData = data => {
-  console.log(data);
-  const main = document.querySelector(".container");
-  data.map(postcard => {
-    console.log(postcard.title);
-    // console.table(data);
-    const postcardContainer = document.createElement("div");
-    postcardContainer.setAttribute("class", "postcard-item");
+const render = {
+  overview: data => {
+    const main = document.querySelector(".container");
+    data.map((postcard, index) => {
+      const postcardContainer = document.createElement("div");
+      postcardContainer.setAttribute("class", "postcard-item");
 
-    const postcardTemplate = `
-		<h1 class="postcard-title">${postcard.title}</h1>
-		<span class="postcard-publication">${postcard.publication}</span>
-		<h3 class="postcard-subject">${postcard.subject}</h3>
-		`;
-    postcardContainer.style.backgroundImage = `url(${postcard.image})`;
-    console.log(postcard.image);
-    postcardContainer.innerHTML = postcardTemplate;
-    main.appendChild(postcardContainer);
-  });
+      const postcardTemplate = `
+				<span class="postcard-publication">${postcard.publication}</span>
+				<h3 class="postcard-subject">${postcard.subject}</h3>
+				<a class="postcard-link" href="#${index}">Test</a>
+				`;
+      postcardContainer.style.background = `
+				linear-gradient(
+		      rgba(0, 0, 0, .25),
+		      rgba(0, 0, 0, .25)
+		    ), url(${postcard.image})
+				`;
+      postcardContainer.innerHTML = postcardTemplate;
+      main.appendChild(postcardContainer);
+    });
+  },
+  detail: data => {
+    console.log(data);
+  }
 };
+
+const router = {
+  init: () => {
+    routie({
+      home: () => {
+        app.call();
+      },
+      detail: detail => {
+        console.log(detail);
+      }
+    });
+  }
+};
+
+router.init();
